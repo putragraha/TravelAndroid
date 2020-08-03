417daed247407f8cfca5b06fb098c513c6f0526f
rengartran <pn.putranugraha@gmail.com>
2020-08-03 07:25:59 +0700

    Migrate data from java library module to android module

--- data/src/main/java/com/neptuunia/data/constant/Source.java
+++ data/src/main/java/com/neptuunia/data/constant/Source.java
@@ -1,9 +1,9 @@
 package com.neptuunia.data.constant;
 
-import android.support.annotation.StringDef;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+
+import androidx.annotation.StringDef;
 
 /**
  * @author nSystem
