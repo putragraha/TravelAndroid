0881016667474a53598487456471407e1131479b
putragraha <pn.putranugraha@gmail.com>
2020-08-08 23:15:19 +0700

    Create Profile User page

--- app/src/main/java/com/neptuunia/travel/homeuser/HomeUserActivity.java
+++ app/src/main/java/com/neptuunia/travel/homeuser/HomeUserActivity.java
@@ -2,21 +2,24 @@ package com.neptuunia.travel.homeuser;
 
 import com.neptuunia.travel.base.BaseActivity;
 import com.neptuunia.travel.databinding.ActivityHomeUserBinding;
+import com.neptuunia.travel.profileuser.ProfileUserActivity;
 
 import android.view.View;
 
 public class HomeUserActivity extends BaseActivity {
 
-    private ActivityHomeUserBinding activityHomeUserBinding;
+    private ActivityHomeUserBinding binding;
 
     @Override
     public View getView() {
-        activityHomeUserBinding = ActivityHomeUserBinding.inflate(getLayoutInflater());
-        return activityHomeUserBinding.getRoot();
+        binding = ActivityHomeUserBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
     }
 
     @Override
     public void setup() {
-        // No implementation
+        binding.btnProfile.setOnClickListener(view ->
+            startActivity(ProfileUserActivity.class)
+        );
     }
 }
---
+++ app/src/main/java/com/neptuunia/travel/profileuser/ProfileUserActivity.java
@@ -0,0 +1,26 @@
+package com.neptuunia.travel.profileuser;
+
+import com.neptuunia.travel.base.BaseActivity;
+import com.neptuunia.travel.databinding.ActivityProfileDriverBinding;
+
+import android.view.View;
+
+/**
+ * @author Putra Nugraha (putra.nugraha@dana.id)
+ * @version ProfileDriverActivity, v 0.0.1 08/08/20 22.48 by Putra Nugraha
+ */
+public class ProfileUserActivity extends BaseActivity {
+
+    private ActivityProfileDriverBinding binding;
+
+    @Override
+    public View getView() {
+        binding = ActivityProfileDriverBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void setup() {
+        // No implementation yet
+    }
+}
