d4ef875c20cfcdefbd3fdac2692f93add5517706
putragraha <pn.putranugraha@gmail.com>
2020-08-08 22:57:52 +0700

    Create Profile Driver page

--- app/src/main/java/com/neptuunia/travel/homedriver/HomeDriverActivity.java
+++ app/src/main/java/com/neptuunia/travel/homedriver/HomeDriverActivity.java
@@ -4,17 +4,18 @@ import com.neptuunia.travel.armadasetting.ArmadaSettingActivity;
 import com.neptuunia.travel.base.BaseActivity;
 import com.neptuunia.travel.databinding.ActivityHomeDriverBinding;
 import com.neptuunia.travel.historydriver.HistoryDriverActivity;
+import com.neptuunia.travel.profiledriver.ProfileDriverActivity;
 
 import android.view.View;
 
 public class HomeDriverActivity extends BaseActivity {
 
-    private ActivityHomeDriverBinding activityHomeDriverBinding;
+    private ActivityHomeDriverBinding binding;
 
     @Override
     public View getView() {
-        activityHomeDriverBinding = ActivityHomeDriverBinding.inflate(getLayoutInflater());
-        return activityHomeDriverBinding.getRoot();
+        binding = ActivityHomeDriverBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
     }
 
     @Override
@@ -23,11 +24,14 @@ public class HomeDriverActivity extends BaseActivity {
     }
 
     private void setupButtonArmadaSetting() {
-        activityHomeDriverBinding.btnArmadaSetting.setOnClickListener(view ->
+        binding.btnArmadaSetting.setOnClickListener(view ->
             startActivity(ArmadaSettingActivity.class)
         );
-        activityHomeDriverBinding.btnHistory.setOnClickListener(view ->
+        binding.btnHistory.setOnClickListener(view ->
             startActivity(HistoryDriverActivity.class)
+        );
+        binding.btnProfile.setOnClickListener(view ->
+            startActivity(ProfileDriverActivity.class)
         );
     }
 }
