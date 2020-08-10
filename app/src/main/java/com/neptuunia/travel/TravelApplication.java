fd3f11e76c7e39814b6be8d2838f3bf9ee2078e2
putragraha <pn.putranugraha@gmail.com>
2020-08-10 07:58:21 +0700

    Remove Rollout implementation

--- app/src/main/java/com/neptuunia/travel/TravelApplication.java
+++ app/src/main/java/com/neptuunia/travel/TravelApplication.java
@@ -1,33 +1,15 @@
 package com.neptuunia.travel;
 
-import com.neptuunia.travel.common.Flags;
-
-import javax.inject.Inject;
-
 import androidx.multidex.MultiDex;
 import androidx.multidex.MultiDexApplication;
 import dagger.hilt.android.HiltAndroidApp;
-import io.rollout.android.Rox;
 
 @HiltAndroidApp
 public class TravelApplication extends MultiDexApplication {
-
-    @Inject
-    Flags flags;
 
     @Override
     public void onCreate() {
         super.onCreate();
         MultiDex.install(this);
-        initRox();
-    }
-
-    public Flags getFlags() {
-        return flags;
-    }
-
-    private void initRox() {
-        Rox.register("", flags);
-        Rox.setup(this);
     }
 }
--- app/src/main/java/com/neptuunia/travel/base/BaseActivity.java
+++ app/src/main/java/com/neptuunia/travel/base/BaseActivity.java
@@ -1,14 +1,10 @@
 package com.neptuunia.travel.base;
 
-import com.neptuunia.travel.R;
-import com.neptuunia.travel.TravelApplication;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
@@ -20,16 +16,11 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (isTravelable()) {
-            setContentView(getView());
-            setup();
-        } else {
-            showRoxMessage();
-        }
+        setContentView(getView());
+        setup();
     }
 
-    public <T> void startActivityWithBundle(Class<T> clazz, Bundle bundle) {
+    protected <T> void startActivityWithBundle(Class<T> clazz, Bundle bundle) {
         Intent intent = new Intent(this, clazz);
         intent.putExtras(bundle);
         startActivity(intent);
@@ -38,19 +29,5 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected <T> void startActivity(Class<T> clazz) {
         Intent intent = new Intent(this, clazz);
         startActivity(intent);
-    }
-
-    private boolean isTravelable() {
-        return ((TravelApplication) getApplication()).getFlags().getTravelable().isEnabled();
-    }
-
-    private void showRoxMessage() {
-        new AlertDialog.Builder(this)
-            .setMessage(R.string.rox_message)
-            .setIcon(R.mipmap.ic_launcher)
-            .setCancelable(false)
-            .setPositiveButton(android.R.string.yes, (dialog, which) -> dialog.cancel())
-            .create()
-            .show();
     }
 }
