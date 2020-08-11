9183eba78f48b488df058a8a2c8f54e99b9b9972
putragraha <pn.putranugraha@gmail.com>
2020-08-11 10:37:14 +0700

    Implement Firebase

--- app/src/main/java/com/neptuunia/travel/base/BaseActivity.java
+++ app/src/main/java/com/neptuunia/travel/base/BaseActivity.java
@@ -1,5 +1,10 @@
 package com.neptuunia.travel.base;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+
+import com.neptuunia.travel.R;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
+    private static final int ONE_MINUTE = 60;
+
+    private FirebaseRemoteConfig firebaseRemoteConfig;
+
     public abstract View getView();
 
     public abstract void setup();
@@ -16,8 +25,7 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(getView());
-        setup();
+        initApps();
     }
 
     protected <T> void startActivityWithBundle(Class<T> clazz, Bundle bundle) {
@@ -29,5 +37,30 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected <T> void startActivity(Class<T> clazz) {
         Intent intent = new Intent(this, clazz);
         startActivity(intent);
+    }
+
+    private void initApps() {
+        firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        firebaseRemoteConfig.setDefaultsAsync(R.xml.firebase_defaults);
+        firebaseRemoteConfig.fetch(ONE_MINUTE)
+            .addOnCompleteListener(this::onFetchComplete);
+    }
+
+    private void onFetchComplete(Task<Void> task) {
+        activateFetch(task);
+        initView();
+    }
+
+    private void activateFetch(Task<Void> task) {
+        if (task.isSuccessful()) {
+            firebaseRemoteConfig.activate();
+        }
+    }
+
+    private void initView() {
+        if (firebaseRemoteConfig.getBoolean(getString(R.string.app_green_light))) {
+            setContentView(getView());
+            setup();
+        }
     }
 }
