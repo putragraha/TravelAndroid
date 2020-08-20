20ff80eaa79e01cd6fccf1233a3ba5d274bdd5fd
putragraha <pn.putranugraha@gmail.com>
2020-08-20 22:24:08 +0700

    Modify OrderTicketActivity style

--- app/src/main/java/com/neptuunia/travel/orderticket/OrderTicketActivity.java
+++ app/src/main/java/com/neptuunia/travel/orderticket/OrderTicketActivity.java
@@ -6,9 +6,15 @@ import com.neptuunia.travel.R;
 import com.neptuunia.travel.base.BaseActivity;
 import com.neptuunia.travel.constant.Constant;
 import com.neptuunia.travel.databinding.ActivityOrderTicketBinding;
+import com.neptuunia.travel.utils.DateTimeUtils;
+import com.neptuunia.travel.utils.ImageUtils;
+import com.neptuunia.travel.utils.NumberUtils;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.Date;
 
 public class OrderTicketActivity extends BaseActivity {
 
@@ -34,18 +40,39 @@ public class OrderTicketActivity extends BaseActivity {
 
         if (ticketResponse != null) {
             Glide.with(this)
-                .load(ticketResponse.getPhotoName())
-                .centerCrop()
+                .load(ImageUtils.getFullUrl(ticketResponse.getPhotoName()))
                 .placeholder(R.mipmap.ic_launcher)
                 .into(binding.acivDriverPicture);
             binding.actvDriverName.setText(ticketResponse.getDriverName());
             binding.actvGroup.setText(ticketResponse.getGroup());
             binding.actvCar.setText(ticketResponse.getCar());
-            binding.actvDepartureTime.setText(ticketResponse.getDatetime());
+            binding.actvDepartureTime.setText(getDatetimeLabel(ticketResponse.getDatetime()));
             binding.actvArmadaClass.setText(ticketResponse.getArmadaClass());
-            binding.actvSeatAvailable.setText(ticketResponse.getSeatAvailable());
-            binding.actvPrice.setText(ticketResponse.getPrice());
+            binding.actvSeatAvailable.setText(getAvailableSeatLabel(ticketResponse));
+            binding.actvPrice.setText(getPriceLabel(ticketResponse.getPrice()));
             binding.actvPhoneNumber.setText(ticketResponse.getDriverPhoneNumber());
         }
+    }
+
+    private String getDatetimeLabel(String datetime) {
+        return DateTimeUtils.getFormattedDatetime(
+            new Date(Long.parseLong(datetime))
+        );
+    }
+
+    private String getAvailableSeatLabel(TicketResponse ticketResponse) {
+        Context context = binding.getRoot().getContext();
+
+        return String.format(
+            context.getString(R.string.amount_of_seat_available),
+            ticketResponse.getSeatAvailable(),
+            ticketResponse.getSeatAmount()
+        );
+    }
+
+    private String getPriceLabel(String price) {
+        return NumberUtils.toRupiah(
+            Integer.parseInt(price)
+        );
     }
 }
