35c498a483ae9b369aa9210543e417c86447e397
putragraha <pn.putranugraha@gmail.com>
2020-08-23 11:21:16 +0700

    Update constant IMAGE_URL path

--- app/src/main/java/com/neptuunia/travel/constant/Constant.java
+++ app/src/main/java/com/neptuunia/travel/constant/Constant.java
@@ -14,7 +14,7 @@ public class Constant {
 
     public static final String API_URL = BuildConfig.BASE_URL + "/project_travel/api/";
 
-    public static final String IMAGE_URL = BuildConfig.BASE_URL + "/project_travel/upload/foto%20driver/";
+    public static final String IMAGE_URL = BuildConfig.BASE_URL + "/project_travel/upload/foto_driver/";
 
     public static final String SP_NAME = "cache";
 
