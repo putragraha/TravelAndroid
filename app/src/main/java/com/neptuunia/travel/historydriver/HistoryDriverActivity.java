3a93cfd221bc5beb84afa2354844bf8797342841
Putra Nugraha <putra.nugraha@dana.id>
2020-09-24 02:57:57 +0700

    Create date filter in history driver page

--- app/src/main/java/com/neptuunia/travel/historydriver/HistoryDriverActivity.java
+++ app/src/main/java/com/neptuunia/travel/historydriver/HistoryDriverActivity.java
@@ -1,5 +1,8 @@
 package com.neptuunia.travel.historydriver;
 
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.MaterialDatePicker;
+
 import com.neptuunia.data.driver.model.response.HistoryDriverResponse;
 import com.neptuunia.travel.R;
 import com.neptuunia.travel.base.BaseActivity;
@@ -7,12 +10,16 @@ import com.neptuunia.travel.common.ViewModelFactory;
 import com.neptuunia.travel.constant.Constant;
 import com.neptuunia.travel.databinding.ActivityHistoryDriverBinding;
 import com.neptuunia.travel.orderdetaildriver.OrderDetailDriverActivity;
+import com.neptuunia.travel.utils.DateTimeUtils;
 
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Date;
+
 import javax.inject.Inject;
 
+import androidx.core.util.Pair;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import dagger.hilt.android.AndroidEntryPoint;
@@ -29,6 +36,8 @@ public class HistoryDriverActivity extends BaseActivity {
 
     private HistoryDriverViewModel historyDriverViewModel;
 
+    private MaterialDatePicker<Pair<Long, Long>> picker;
+
     @Override
     public View getView() {
         binding = ActivityHistoryDriverBinding.inflate(getLayoutInflater());
@@ -39,7 +48,10 @@ public class HistoryDriverActivity extends BaseActivity {
     public void setup() {
         setupRecylerView();
         initHistoryDriverViewModel();
+        initMaterialDatePicker();
+        setupDatePickerPositiveListener();
         setupToolbar();
+        setupDateFilter();
         getHistoryDrivers();
         setupOnSuccessGetHistoryDrivers();
     }
@@ -55,9 +67,53 @@ public class HistoryDriverActivity extends BaseActivity {
             .get(HistoryDriverViewModel.class);
     }
 
+    private void initMaterialDatePicker() {
+        CalendarConstraints calendarConstraints = new CalendarConstraints.Builder()
+            .build();
+
+        picker = MaterialDatePicker.Builder
+            .dateRangePicker()
+            .setCalendarConstraints(calendarConstraints)
+            .build();
+    }
+
+    private void setupDatePickerPositiveListener() {
+        picker.addOnPositiveButtonClickListener(selection -> {
+            if (selection == null || selection.first == null || selection.second == null) {
+                return;
+            }
+
+            long minDate = selection.first;
+            long maxDate = selection.second + DateTimeUtils.ONE_DAY_IN_MILLIS;
+
+            historyDriverViewModel.filterHistoryDrivers(minDate, maxDate);
+            binding.viewFilterDate.actvDate.setText(
+                getString(
+                    R.string.date_range_message,
+                    DateTimeUtils.getFormattedDate(new Date(selection.first)),
+                    DateTimeUtils.getFormattedDate(new Date(selection.second))
+                )
+            );
+        });
+    }
+
     private void setupToolbar() {
         binding.viewToolbar.actvTitle.setText(R.string.history);
         binding.viewToolbar.acivArrowBack.setOnClickListener(view -> onBackPressed());
+    }
+
+    private void setupDateFilter() {
+        binding.viewFilterDate.clFilterDate.setOnClickListener(this::openDateRangePicker);
+        binding.viewFilterDate.acivCancel.setOnClickListener(this::clearDateRangeFilter);
+    }
+
+    private void openDateRangePicker(View view) {
+        picker.show(getSupportFragmentManager(), picker.toString());
+    }
+
+    private void clearDateRangeFilter(View view) {
+        binding.viewFilterDate.actvDate.setText(getString(R.string.date_range));
+        historyDriverViewModel.clearHistoryDrivers();
     }
 
     private void getHistoryDrivers() {
