6a6b3a7ffea3aa5b1e5239707974396e61344fb8
Putra Nugraha <putra.nugraha@dana.id>
2020-09-24 02:48:47 +0700

    Create date filter at history user page

--- app/src/main/java/com/neptuunia/travel/historyuser/HistoryUserActivity.java
+++ app/src/main/java/com/neptuunia/travel/historyuser/HistoryUserActivity.java
@@ -1,5 +1,8 @@
 package com.neptuunia.travel.historyuser;
 
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.MaterialDatePicker;
+
 import com.neptuunia.data.user.model.response.HistoryUserResponse;
 import com.neptuunia.travel.R;
 import com.neptuunia.travel.base.BaseActivity;
@@ -7,12 +10,16 @@ import com.neptuunia.travel.common.ViewModelFactory;
 import com.neptuunia.travel.constant.Constant;
 import com.neptuunia.travel.databinding.ActivityHistoryUserBinding;
 import com.neptuunia.travel.orderdetailuser.OrderDetailUserActivity;
+import com.neptuunia.travel.utils.DateTimeUtils;
 
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Date;
+
 import javax.inject.Inject;
 
+import androidx.core.util.Pair;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import dagger.hilt.android.AndroidEntryPoint;
@@ -29,6 +36,8 @@ public class HistoryUserActivity extends BaseActivity {
 
     private HistoryUserViewModel historyUserViewModel;
 
+    private MaterialDatePicker<Pair<Long, Long>> picker;
+
     @Override
     public View getView() {
         binding = ActivityHistoryUserBinding.inflate(getLayoutInflater());
@@ -39,7 +48,10 @@ public class HistoryUserActivity extends BaseActivity {
     public void setup() {
         setupRecylerView();
         initHistoryDriverViewModel();
+        initMaterialDatePicker();
+        setupDatePickerPositiveListener();
         setupToolbar();
+        setupDateFilter();
         getHistoryUsers();
         setupOnSuccessGetHistoryUser();
     }
@@ -61,9 +73,53 @@ public class HistoryUserActivity extends BaseActivity {
             .get(HistoryUserViewModel.class);
     }
 
+    private void initMaterialDatePicker() {
+        CalendarConstraints calendarConstraints = new CalendarConstraints.Builder()
+            .build();
+
+        picker = MaterialDatePicker.Builder
+            .dateRangePicker()
+            .setCalendarConstraints(calendarConstraints)
+            .build();
+    }
+
+    private void setupDatePickerPositiveListener() {
+        picker.addOnPositiveButtonClickListener(selection -> {
+            if (selection == null || selection.first == null || selection.second == null) {
+                return;
+            }
+
+            long minDate = selection.first;
+            long maxDate = selection.second + DateTimeUtils.ONE_DAY_IN_MILLIS;
+
+            historyUserViewModel.filterHistoryUsers(minDate, maxDate);
+            binding.viewFilterDate.actvDate.setText(
+                getString(
+                    R.string.date_range_message,
+                    DateTimeUtils.getFormattedDate(new Date(selection.first)),
+                    DateTimeUtils.getFormattedDate(new Date(selection.second))
+                )
+            );
+        });
+    }
+
     private void setupToolbar() {
         binding.viewToolbar.actvTitle.setText(R.string.history);
         binding.viewToolbar.acivArrowBack.setOnClickListener(view -> onBackPressed());
+    }
+
+    private void setupDateFilter() {
+        binding.viewFilterDate.clFilterDate.setOnClickListener(this::openDateRangePicker);
+        binding.viewFilterDate.acivCancel.setOnClickListener(this::clearDateRangeFilter);
+    }
+
+    private void openDateRangePicker(View view) {
+        picker.show(getSupportFragmentManager(), picker.toString());
+    }
+
+    private void clearDateRangeFilter(View view) {
+        binding.viewFilterDate.actvDate.setText(getString(R.string.date_range));
+        historyUserViewModel.clearHistoryUsers();
     }
 
     private void getHistoryUsers() {
--- app/src/main/java/com/neptuunia/travel/historyuser/HistoryUserViewModel.java
+++ app/src/main/java/com/neptuunia/travel/historyuser/HistoryUserViewModel.java
@@ -7,6 +7,7 @@ import com.neptuunia.travel.utils.Transformer;
 
 import android.app.Application;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -22,6 +23,8 @@ public class HistoryUserViewModel extends AndroidViewModel {
         new MutableLiveData<>();
 
     private UserRepository userRepository;
+
+    private List<HistoryUserResponse> historyUserResponses = new ArrayList<>();
 
     @Inject
     public HistoryUserViewModel(
@@ -44,8 +47,27 @@ public class HistoryUserViewModel extends AndroidViewModel {
                 @Override
                 public void onSuccess(List<HistoryUserResponse> historyUserResponses) {
                     super.onSuccess(historyUserResponses);
+                    HistoryUserViewModel.this.historyUserResponses = historyUserResponses;
                     historyUserResponseLiveData.postValue(historyUserResponses);
                 }
             });
+    }
+
+    public void filterHistoryUsers(long minDate, long maxDate) {
+        List<HistoryUserResponse> results = new ArrayList<>();
+
+        for (HistoryUserResponse historyUserResponse : historyUserResponses) {
+            long datetime = Long.parseLong(historyUserResponse.getDatetime());
+
+            if (datetime >= minDate && datetime <= maxDate) {
+                results.add(historyUserResponse);
+            }
+        }
+
+        historyUserResponseLiveData.postValue(results);
+    }
+
+    public void clearHistoryUsers() {
+        historyUserResponseLiveData.postValue(historyUserResponses);
     }
 }
